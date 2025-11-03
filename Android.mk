b171db763cb22d78b31ceab5dfff707d359e6db3
Muhammad Fadly Saripudin <mdigreget@gmail.com>
2025-11-03 17:35:51 +0000

    beryl: uprev vendor from OS2.0.207.0.VOQMIXM

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),beryl)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,c5ed808494caa4fcbb3a0ef550354ed8b9aee747)
+$(call add-radio-file-sha1-checked,radio/gz.img,fb682640778cf60f912abc367068c055af0774c5)
+$(call add-radio-file-sha1-checked,radio/lk.img,1a83f35d57fb0e2ac8d83d7d96dc57654aa528c0)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,ec1d503c6fee49c79882491f27d13d62064975c1)
+$(call add-radio-file-sha1-checked,radio/md1img.img,122747d13c92db30bb6d5dad3a79ee67c90fa160)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,2c24e1dd30d857f4dc1a30f90909c24729e4b724)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,df664d960ffd95458505495ec225ab69a9b7662c)
+$(call add-radio-file-sha1-checked,radio/scp.img,0b758a2d2254b5531e5351c0788a0da90a2b49e2)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,67728150e38811814e840c9a06f8d90f4a35efbf)
+$(call add-radio-file-sha1-checked,radio/sspm.img,385f192e535305965b293a5d9d6fb352f149d480)
+$(call add-radio-file-sha1-checked,radio/tee.img,eaf2484a413c93d056ec1f8d3b8d2843fffb661c)
+
+endif
