930e451994a5df0b1c3963d1135bab267574feb2
Naoko Shoto <me@naokoshoto.my.id>
2026-01-28 09:58:14 +0000

    beryl: State Git-LFS
    Change-Id: I4459a1d87c661643dd021d7340fec73d1ad4cd53

--- Android.mk
+++ Android.mk
@@ -1,21 +1,3 @@
-#
-# Automatically generated file. DO NOT MODIFY
-#
-
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),beryl)
-
-$(call add-radio-file-sha1-checked,radio/dpm.img,8b9f715f552607c9e8727c814fef6c227a814d8f)
-$(call add-radio-file-sha1-checked,radio/gz.img,dd4e6722dcb47877b9d984c3dbdc05b215303de0)
-$(call add-radio-file-sha1-checked,radio/lk.img,037f684033e8ad2cfd9ffbd1d31105ca2d61070d)
-$(call add-radio-file-sha1-checked,radio/mcupm.img,81d59abd7f316de666d3f3187b874a96201dbcbd)
-$(call add-radio-file-sha1-checked,radio/md1img.img,407322ea2a5cbd256e161e2bcf24a94fae055127)
-$(call add-radio-file-sha1-checked,radio/pi_img.img,c65289b83caaf02d43d13ddbfe3f8093cb6a5b95)
-$(call add-radio-file-sha1-checked,radio/preloader_raw.img,3dc2eccf1db16191188b4b844857188d4cdda473)
-$(call add-radio-file-sha1-checked,radio/scp.img,ccbe1afad64a8c21c7ed00277948ab78d1abc103)
-$(call add-radio-file-sha1-checked,radio/spmfw.img,a37f42f175eecca9f90057435afe7e0e150c74b0)
-$(call add-radio-file-sha1-checked,radio/sspm.img,c4ed771eed69cb723af38f4ff713a9bcab7c290e)
-$(call add-radio-file-sha1-checked,radio/tee.img,75de6577e67c11c16dd9cdd59e0816f7bbf38fa6)
-
-endif
+version https://git-lfs.github.com/spec/v1
+oid sha256:301084e4d5e8a0a90fb888b0b4c349f0c39d9290b0cd6bd20b416268052f9379
+size 1139
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,16 +1,3 @@
-#
-# Automatically generated file. DO NOT MODIFY
-#
-
-AB_OTA_PARTITIONS += \
-    dpm \
-    gz \
-    lk \
-    mcupm \
-    md1img \
-    pi_img \
-    preloader_raw \
-    scp \
-    spmfw \
-    sspm \
-    tee
+version https://git-lfs.github.com/spec/v1
+oid sha256:236d009f2fa86e49c56f731f15f964597926cc0014c0273f4d2e625f30bd0499
+size 201
