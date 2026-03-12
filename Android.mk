9865cb9208b4e2ef14fc31bf8c5cf2ba84e64c2a
Naoko Shoto <me@naokoshoto.my.id>
2026-03-12 11:13:19 +0100

    fixup! beryl-kernel: Update from OS2.0.210.0.VOQMIXM

--- Android.mk
+++ Android.mk
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ee3461427a27b3772074a50c34c61246909577fbb3eceb2504e73aa048daeb17
-size 1139
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),beryl)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,24c8c81a0481e19dc6106efa2b492126d8932ed1)
+$(call add-radio-file-sha1-checked,radio/gz.img,f6f370ebefeebcf46a2458228a51f3394345493e)
+$(call add-radio-file-sha1-checked,radio/lk.img,2155d68fed49c35c6bb7d3ea71262651669e2f4d)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,03d3fa550c0722c485e98d061116b5876dc5bb96)
+$(call add-radio-file-sha1-checked,radio/md1img.img,7d660341cca4e8cb8339a755960ea8a349c17f44)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,52e4827a29f58bc03a4f0909bd867dc782829c88)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,23e7193e01261a486a75995c7e3509322084c9f8)
+$(call add-radio-file-sha1-checked,radio/scp.img,e2dd9f3bc8efb8a3aad5f7f2ae0c7fa55d4b96ee)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,4a723125301cd6ea3cf36bd9eb7d795c6938bffb)
+$(call add-radio-file-sha1-checked,radio/sspm.img,08aaf512bfe26bc12d07c503331a3dec5d25dbfa)
+$(call add-radio-file-sha1-checked,radio/tee.img,2c638354de49c6f10038fffd1730d8b621a2e5fb)
+
+endif
