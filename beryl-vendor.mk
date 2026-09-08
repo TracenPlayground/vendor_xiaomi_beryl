2ee4e6787f3be14fc56fd1509869e96ac73a158a
Naoko Shoto <me@naokoshoto.my.id>
2026-09-08 19:25:19 +0200

    beryl: Update blobs and restore original AIDL dependencies for audio and bluetooth
    Change-Id: Idbd02e91aa5dd6b0e6dddb9c9c391a0fe49288a1
    Signed-off-by: Naoko Shoto <me@naokoshoto.my.id>

--- beryl-vendor.mk
+++ beryl-vendor.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97428adc2ed34541acc0ce8ca9e8510c0a277278a6862538da74e1c6e9bfcee2
-size 236830
+oid sha256:e6a7ed81a54dbe941243927151f3a9d197b32592670542ffc628bd66b6bc1f22
+size 236845
